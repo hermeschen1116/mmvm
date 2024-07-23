4a40f8e50ba6ab4175900d6cd292a584857f36c9
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-07-23 13:58:45 +0900

    WIP

---
+++ run.sh
@@ -0,0 +1,14 @@
+cargo run -r -- -d ./testcases/1.out
+cargo run -r -- -m ./testcases/1.out
+cargo run -r -- -d ./testcases/2.out
+cargo run -r -- -m ./testcases/2.out
+cargo run -r -- -d ./testcases/3.out
+cargo run -r -- -m ./testcases/3.out
+cargo run -r -- -d ./testcases/4.out
+cargo run -r -- -m ./testcases/4.out
+cargo run -r -- -d ./testcases/5.out
+cargo run -r -- -m ./testcases/5.out
+cargo run -r -- -d ./testcases/6.out
+cargo run -r -- -m ./testcases/6.out
+cargo run -r -- -d ./testcases/7.out
+cargo run -r -- -m ./testcases/7.out
